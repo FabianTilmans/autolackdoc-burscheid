156dfe5353b9ccd89f61cb56cd6c4d04ce547dbe
FabianTil <fabian.tilmans@edu.fhdw.de>
2025-09-03 21:58:00 +0200

    Initial Commit

---
+++ src/components/sections/leistungen/data.ts
@@ -0,0 +1,59 @@
+import img1 from "./assets/Autorep-Spotrepair-klein.jpg";
+import img2 from "./assets/Unfallinstandsetzung.jpg";
+// TODO
+import img3 from "./assets/Polierpad-und-Politur.jpg";
+import img4 from "./assets/hero-intro-carwrapping2-woerle-marketing.jpg"
+// TODO
+import img5 from "./assets/lackierung.jpg";
+import img6 from "./assets/lackversieglung.jpg";
+
+export const servicesSectionData = {
+  title: "Leistungen",
+  subheading: {
+    text1_1: "Perfektion, die man sieht. Präzision, die bleibt.",
+    text1_2: "Ihr Partner für hochwertige Kfz-Lackierungen",
+    text2:
+      "Als Experte für Lakierungen im KFZ-Bereich verhelfen wir jedem Fahrzeug zu neuem Glanz und einem makellosen Finish.",
+  },
+  services: [
+    {
+      title: "Spot-Repair",
+      briefDescription:
+        "Die kostengünstige und schnelle Reparatur kleiner Lackschäden wie Kratzer und Steinschläge, ohne ein ganzes Bauteil neu lackieren zu müssen.",
+      visual: img1,
+    },
+    {
+      title: "Unfallinstandsetzung",
+      briefDescription:
+        "Beseitigung von Lackschäden nach einem Unfall, oft in Zusammenarbeit mit Karosseriebauern. Dies umfasst das Entfernen von Beulen, das Spachteln und die Neulackierung beschädigter Teile.",
+      visual: img2,
+    },
+    {
+      title: "Folierung",
+      briefDescription:
+        "Neuer Look, gezielte Akzente setzen oder den Originallack schützen – Folierungen sind die perfekte Lösung und eine Alternative zur Lackierung.",
+      visual: img4,
+    },
+    {
+      title: "Komplettlackierung",
+      briefDescription:
+        "Die Neulackierung des gesamten Fahrzeugs, sei es zur Farbänderung, zur Beseitigung von Alterungsspuren oder zur Wiederherstellung nach einem Totalschaden.",
+      visual: img5,
+    },
+    {
+      title: "Lackversiegelung und -schutz",
+      briefDescription:
+        "Das Auftragen von speziellen Versiegelungen (z. B. Keramikversiegelung) zum Schutz des Lackes vor Umwelteinflüssen und kleinen Kratzern",
+      visual: img6,
+    },
+    {
+      title: "Lackpolitur",
+      briefDescription:
+        "Entfernung von feinen Kratzern und Hologrammen, um den Lack wieder zum Glänzen zu bringen.",
+      visual: img3,
+    },
+  ],
+  callToAction:
+    "Discover the Future of Green Energy. Choose Your Sustainable Solution Today!",
+  buttonText: "Explore More",
+};
---
+++ src/components/sections/projekte/data.ts
@@ -0,0 +1,24 @@
+import img1 from "./assets/moto_guzzi.jpg";
+import img2 from "./assets/bmw1er.jpg";
+
+export const projectsSection = {
+  title: "Projekte",
+  subheading1_1: "Lassen Sie sich von unseren Projekten",
+  subheading1_2: "inspirieren",
+  projects: [
+    {
+      title: "Moto Guzzi Daytona 1000",
+      description:
+        "Diese grüne Moto Guzzi Rennmaschine mit der Startnummer 399 ist ein beeindruckendes Beispiel für ein klassisches Rennmotorrad, das für die Rennstrecke optimiert wurde.",
+      location: "",
+      visual: img1,
+    },
+    {
+      title: "BMW 1er, BJ 2010",
+      description:
+        "Mit ihrer neuen, makellosen Lackierung erstrahlt die BMW 1er-Reihe in neuem Glanz, als wäre sie gerade erst vom Band gerollt.",
+      location: "",
+      visual: img2,
+    },
+  ],
+};
